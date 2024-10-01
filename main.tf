69d8db27ebdfb4bfb155c0d2c48fc3efd0d53e3c
Victor-Borges-Silva <victorborges975@gmail.com>
2024-09-30 21:03:57 -0300

    incluindo role de backup

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# Politica e Role para instancias EC2
 # tfsec:ignore:aws-iam-no-policy-wildcards
 resource "aws_iam_policy" "Inicia_Desliga_EC2" {
   name        = var.policy_name
@@ -48,4 +49,30 @@ resource "aws_iam_role_policy_attachment" "anexar_politica_Inicia_Desliga_EC2_pe
 resource "aws_iam_role_policy_attachment" "anexar_politica_Inicia_Desliga_EC2_basica" {
   role       = aws_iam_role.Inicia_Desliga_EC2.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# Politica e Role para AWSBackup
+# Recurso do IAM Role para o plano de backup
+resource "aws_iam_role" "role_backup" {
+  name        = var.nome_role_backup
+  description = var.description_role_backup
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "backup.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
+
+  # Anexa a política gerenciada necessária para backup
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForBackup",
+    "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForRestores"
+  ]
 }
--- variables.tf
+++ variables.tf
@@ -19,3 +19,17 @@ variable "role_description" {
   type    = string
   default = "Default_Varibletf"
 }
+
+
+#IAM Role para o plano de backup
+variable "nome_role_backup" {
+  type        = string
+  default     = "Default_TF"
+  description = "Nome da role/função para permitir o AWSBackup gerenciar os pontos de restauração"
+}
+
+variable "description_role_backup" {
+  type        = string
+  default     = "Default_TF"
+  description = "Descrição para função IAM que permite o cofre gerenciar os backups"
+}
