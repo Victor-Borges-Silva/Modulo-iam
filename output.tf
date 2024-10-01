9ebacc098ea30071a512ab0e10dbd0db1f322c59
Victor-Borges-Silva <victorborges975@gmail.com>
2024-09-30 21:47:57 -0300

    Ajuste no output para exportar o arn da role de backup

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
-output "iam_role_arn" {
+output "iam_role_arn_ec2" {
   value = aws_iam_role.Inicia_Desliga_EC2.arn
-}    
+}
+
+output "iam_role_arn_backup" {
+  value = aws_iam_role.role_backup.arn
+}  
